b7fac57015b98c17b65f687d1f504fc19b1d2a9d
val-po <valentine.potapov@gmail.com>
2023-05-15 15:28:28 +0300

    commit #1

--- ChartWithUIKit/ViewController.swift
+++ ChartWithUIKit/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createMyChart()
     }
 
-
+    private func createMyChart() {
+        let barChart = BarChartView(frame: CGRect(x: 0,
+                                                  y: 0,
+                                                  width: view.frame.size.width,
+                                                  height: view.frame.size.width))
+        barChart.center = view.center
+        // Axes
+        let xAxis = barChart.xAxis
+        let rightAxis = barChart.rightAxis
+        // Legend
+        let legend = barChart.legend
+        var entries: [BarChartDataEntry] = []
+        for x in 0..<8 {
+            entries.append(BarChartDataEntry(x: Double(x), y: Double.random(in: 0...50)))
+        }
+        let set = BarChartDataSet(entries: entries, label: "Values")
+        set.colors = ChartColorTemplates.vordiplom()
+        let chartData = BarChartData(dataSets: [set])
+        barChart.data = chartData
+        view.addSubview(barChart)
+    }
 }
 
